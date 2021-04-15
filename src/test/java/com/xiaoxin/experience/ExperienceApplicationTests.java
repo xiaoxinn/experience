254450909aa17adbdf96de571f6904e4b849b0fb
肖鑫 <512762867@qq.com>
2021-04-15 22:25:22 +0800

    编写测试类

--- src/test/java/com/xiaoxin/experience/ExperienceApplicationTests.java
+++ src/test/java/com/xiaoxin/experience/ExperienceApplicationTests.java
@@ -1,32 +1,43 @@
 package com.xiaoxin.experience;
 
-import com.xiaoxin.experience.bean.Car;
-import com.xiaoxin.experience.bean.CarDto;
-import com.xiaoxin.experience.mapper.CarMapper;
 import org.junit.Assert;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.*;
+import org.springframework.http.client.ClientHttpRequest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.RequestCallback;
+import org.springframework.web.client.ResponseExtractor;
+import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.io.IOException;
+import java.util.Collection;
+import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 /**
  * create by xiaoxin on 2021/3/19
  */
-@SpringBootTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = {ExperienceApplication.class})
 public class ExperienceApplicationTests {
 
+    @Autowired
+    private RestTemplate restTemplate;
 
     @Test
-    public void mapstructStart()
+    public void restTemplateTest()
     {
-        //given
-        Car car = new Car( "Morris", "5", System.currentTimeMillis() + "" );
-
-        //when
-        CarDto carDto = CarMapper.INSTANCE.carToCarDto( car );
-
-        //then
-        Assert.assertNotNull(carDto);
-        Assert.assertEquals("Morris",carDto.getName());
-        Assert.assertEquals("5",carDto.getPrice());
-        Assert.assertNotNull(carDto.getCreateTime());
+        String url  = "https://114.116.223.94:10013/api/system/version";
+        HttpHeaders header = new HttpHeaders();
+        header.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        HttpEntity<String> httpEntity  = new HttpEntity<>("{}",header);
+        ResponseEntity<String> exchange = restTemplate.exchange(url, HttpMethod.POST, httpEntity, String.class);
+        System.out.println(exchange.getBody());
     }
 }
