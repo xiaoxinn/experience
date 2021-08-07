d7c1600d7a3618bcad68bd6040351919b9da0108
肖鑫 <512762867@qq.com>
2021-08-07 19:36:19 +0800

    改写文件下载

--- src/main/java/com/xiaoxin/experience/util/DownloadUtil.java
+++ src/main/java/com/xiaoxin/experience/util/DownloadUtil.java
@@ -1,8 +1,11 @@
 package com.xiaoxin.experience.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.io.*;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.Objects;
 
 /**
  * <p>Simple life, simple work, don't complicate</p>
@@ -11,6 +14,7 @@ import java.net.URL;
  * @version 1.0.0
  * @date 2021/7/17
  */
+@Slf4j
 public class DownloadUtil {
 
     private DownloadUtil() {}
@@ -18,40 +22,74 @@ public class DownloadUtil {
     /**
      * 从网络Url中下载文件
      */
-    public static void downLoadFromUrl(String sourceFileUrl, String fileName, String savePath) throws IOException {
-        URL url = new URL(sourceFileUrl);
-        HttpURLConnection conn = (HttpURLConnection) url.openConnection();
-        //设置超时间为3秒
-        conn.setConnectTimeout(3 * 1000);
-        //防止屏蔽程序抓取而返回403错误
-        conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
-
-        //得到输入流
-        InputStream inputStream = conn.getInputStream();
-        //获取自己数组
-        byte[] getData = readInputStream(inputStream);
-
-        //文件保存位置
-        File saveDir = new File(savePath);
-        if (!saveDir.exists())
+    public static void downLoadFromUrl(String sourceFileUrl, String fileName, String savePath)
+    {
+        log.debug("start download ...");
+        InputStream inputStream = null;
+        FileOutputStream fos = null;
+        try
         {
-            boolean mkdirs = saveDir.mkdirs();
-            if (!mkdirs)
+            URL url = new URL(sourceFileUrl);
+            HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+            //设置超时间为3秒
+            conn.setConnectTimeout(3 * 1000);
+            //防止屏蔽程序抓取而返回403错误
+            conn.setRequestProperty("User-Agent", "Mozilla/4.0 (compatible; MSIE 5.0; Windows NT; DigExt)");
+            //得到输入流
+            inputStream = conn.getInputStream();
+            //获取自己数组
+            byte[] getData = readInputStream(inputStream);
+
+            //文件保存位置
+            File saveDir = new File(savePath);
+            if (!saveDir.exists()) {
+                boolean mkdirs = saveDir.mkdirs();
+                if (!mkdirs)
+                {
+                    log.error("create dirs fail, download error");
+                    return;
+                }
+            }
+            File file = new File(saveDir + File.separator + fileName);
+            fos = new FileOutputStream(file);
+            fos.write(getData);
+            log.debug("down load file success !");
+        }
+        catch (IOException e)
+        {
+            log.error("down fail ...");
+        }
+        finally
+        {
+            if (Objects.nonNull(inputStream))
             {
-                throw new RuntimeException("create dirs fail");
+                try
+                {
+                    inputStream.close();
+                }
+                catch (IOException e)
+                {
+                    log.error("close input stream fail");
+                }
+            }
+            if (Objects.nonNull(fos))
+            {
+                try
+                {
+                    fos.close();
+                }
+                catch (IOException e)
+                {
+                    log.error("close write stream fail");
+                }
             }
         }
-        File file = new File(saveDir + File.separator + fileName);
-        FileOutputStream fos = new FileOutputStream(file);
-        fos.write(getData);
-        fos.close();
-        inputStream.close();
     }
 
     /**
      * 从输入流中获取字节数组
      */
-    public static byte[] readInputStream(InputStream inputStream)
+    private static byte[] readInputStream(InputStream inputStream)
             throws IOException
     {
         byte[] buffer = new byte[1024];
